f8254054a8b3eb010fec603104eebfe7e011d3e5
elifgn <efagnon2002@gmail.com>
2022-07-26 02:04:59 +0100

    adding getUser by email and delete user

---
+++ src/main/java/com/example/jeeproject/controllers/RegisterServlet.java
@@ -0,0 +1,21 @@
+package com.example.jeeproject.controllers;
+
+import com.example.jeeproject.models.User;
+import com.example.jeeproject.services.UserService;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", value = "/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
--- src/main/java/com/example/jeeproject/services/UserService.java
+++ src/main/java/com/example/jeeproject/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.jeeproject.models.User;
 import com.example.jeeproject.repos.DbUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
 public class UserService {
     private final static EntityManager entityManager = DbUtil.getEntityManager("default");
@@ -15,6 +16,38 @@ public class UserService {
         try {
             transaction.begin();
             entityManager.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public static User getUser(String email) {
+        String query = "SELECT u FROM User u WHERE u.email = :email";
+        TypedQuery<User> typedQuery = entityManager.createQuery(query, User.class);
+        typedQuery.setParameter("email", email);
+        User user = null;
+
+        try {
+            user = typedQuery.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return user;
+    }
+
+    public static void deleteUser(String email) {
+        User user = getUser(email);
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+            entityManager.remove(user);
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
